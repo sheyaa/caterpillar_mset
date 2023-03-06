81f783d2a1eecf43064f27f9667233b3623bd028
Yodd Mena <taekidoyodd@live.com>
2023-03-06 14:25:56 -0800

    Created Mice_SVM
    SInce I already created an SMV model using synthetic data, I approached the mice data similarly

--- example.py
+++ example.py
@@ -7,12 +7,13 @@ halfway through the observation period.
 
 import numpy as np
 import mset
+import tde
 
-n = 10000
-thresh = 0.1
+n = 10000 # making 10000 iterations 
+thresh = 0.1 # threshold of 0.1
 
-t = np.linspace(0, 10*np.pi, n)
-x = np.sin(t) + 0.1*np.random.randn(n)
+t = np.linspace(0, 10*np.pi, n) #function to generate a general sequence out of the range of numbers provided 
+x = np.sin(t) + 0.1*np.random.randn(n) #sine funtion with noise added 
 
 # introduce a new type of signal halfway through.
 heavy = (1 + np.tanh( 0.5*(t-5*np.pi) ))/2.
@@ -25,7 +26,8 @@ x += heavy*np.sin(t/2) + heavy*(-np.sin(t))
 # sufficient to get a numerical zero-autocorrelation time.
 delay = int((np.pi/2)/(t[1] - t[0]))
 
-ms = mset.MSET()
+ms = mset.MSET() # mset module 
+X = tde.tde(x) #synthetic dataset 
 fig,ax = ms.visualize_mset(x,thresh,delay, verbosity=1)
 fig.show()
 
